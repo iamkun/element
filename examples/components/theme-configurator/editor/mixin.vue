b17d5a05b9689449541c4cebc1229a752b778aca
iamkun <kunhello@outlook.com>
2019-01-23 15:57:05 +0800

    chore: update style

---
+++ examples/components/theme-configurator/editor/mixin.vue
@@ -0,0 +1,22 @@
+<style lang="postcss">
+    .config {
+      padding: 10px 0;
+    }
+    .config-label {
+      color: #666;
+      font-size: 14px;
+      padding-bottom: 5px;
+    }
+    .config-content {
+
+    }
+    .content-80 {
+      display: inline-block;
+      width: 80%;
+    }
+    .content-20 {
+      display: inline-block;
+      width: 20%;
+      vertical-align: bottom;
+    }
+</style>
