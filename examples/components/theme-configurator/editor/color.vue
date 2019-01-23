05427a10a563e29f67b34888821e79b323247b02
iamkun <kunhello@outlook.com>
2019-01-23 15:57:05 +0800

    chore: update style

--- examples/components/theme-configurator/editor/color.vue
+++ examples/components/theme-configurator/editor/color.vue
@@ -1,62 +1,85 @@
 <template>
-  <div :key="displayName">
-    <span>{{displayName}}</span>
-    <el-popover
-      ref="popover"
-      placement="top-start"
-      :trigger="popoverTrigger"
-    >
-      <ul
-      class="color-list"
-      >
-        <li
-          v-for="(color, key) in golbalColorList" 
-          :key="key"
-          :style="{ 
-            backgroundColor: color.color,
-            color: color.colorInvert
-          }"
-          @click="onListColorClick(color)"
-          class="list-item"
+  <section class="config" :key="displayName">
+    <div class="config-label">
+      {{displayName}}
+    </div>
+    <div class="config-content">
+      <div class="content-80">
+        <el-popover
+          ref="popover"
+          placement="top-start"
+          :trigger="popoverTrigger"
         >
-          {{color.label}}
-          {{color.color}}
-        </li>
-      </ul>
-      <el-input
-        style="width: 200px"
-        :value=displayValue
-        readonly
-        slot="reference"
-      ></el-input>
-    </el-popover>
-    <el-color-picker 
-      class="colorPicker"
-      v-model="pickerColor" 
-      @change=onPickerChange
-    ></el-color-picker>
-  </div>
+          <ul
+          class="color-list"
+          >
+            <li
+              v-for="(color, key) in golbalColorList" 
+              :key="key"
+              :style="{ 
+                backgroundColor: color.color,
+                color: color.colorInvert
+              }"
+              @click="onListColorClick(color)"
+              class="list-item"
+            >
+              {{color.label}}
+              {{color.color}}
+            </li>
+          </ul>
+          <el-input
+            :value=displayValue
+            readonly
+            slot="reference"
+          ></el-input>
+        </el-popover>
+      </div>
+      <div class="content-20">
+        <el-color-picker 
+          class="colorPicker"
+          v-model="pickerColor" 
+          @change=onPickerChange
+        ></el-color-picker>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="postcss">
+
 .colorPicker {
-  vertical-align: bottom;
   margin-left: 10px;
+  vertical-align: bottom;
 }
 .color-list {
   margin: 0;
   padding: 0;
   list-style: none;
+  max-height: 400px;
+  overflow-x: auto;
   .list-item {
+    border: 1px solid #eee;
+    margin: 3px;
+    text-align: center;
+    line-height: 40px;
     height: 40px;
+    border-radius: 3px;
+    font-size: 12px;
+    cursor: pointer;
+    &:hover {
+      opacity: .8;
+      box-shadow: 0 0 8px 0 #eee;
+    }
   }
 }
 </style>
 
 <script>
+import Mixin from './mixin';
 import { getColorFromName, getStyleDisplayName, invertColor } from '../utils/utils.js';
 
 export default {
+  mixins: [Mixin],
   props: {
     config: {
       type: Object
