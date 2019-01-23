21b858466d7a44017ca8e3e88969fe77b7483c0f
iamkun <kunhello@outlook.com>
2019-01-23 15:21:01 +0800

    fix: init color list item

--- examples/components/theme-configurator/editor/color.vue
+++ examples/components/theme-configurator/editor/color.vue
@@ -1,23 +1,60 @@
 <template>
-  <div >
+  <div :key="displayName">
     <span>{{displayName}}</span>
-    <el-input
-      :value=displayValue
-    ></el-input>
+    <el-popover
+      ref="popover"
+      placement="top-start"
+      :trigger="popoverTrigger"
+    >
+      <ul
+      class="color-list"
+      >
+        <li
+          v-for="(color, key) in golbalColorList" 
+          :key="key"
+          :style="{ 
+            backgroundColor: color.color,
+            color: color.colorInvert
+          }"
+          @click="onListColorClick(color)"
+          class="list-item"
+        >
+          {{color.label}}
+          {{color.color}}
+        </li>
+      </ul>
+      <el-input
+        style="width: 200px"
+        :value=displayValue
+        readonly
+        slot="reference"
+      ></el-input>
+    </el-popover>
     <el-color-picker 
-      v-model="color" 
-      size="mini"
+      class="colorPicker"
+      v-model="pickerColor" 
       @change=onPickerChange
     ></el-color-picker>
   </div>
 </template>
 
-<style>
-.main {}
+<style lang="postcss">
+.colorPicker {
+  vertical-align: bottom;
+  margin-left: 10px;
+}
+.color-list {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  .list-item {
+    height: 40px;
+  }
+}
 </style>
 
 <script>
-import { getColorFromName, getStyleDisplayName } from '../utils/utils.js';
+import { getColorFromName, getStyleDisplayName, invertColor } from '../utils/utils.js';
 
 export default {
   props: {
@@ -36,7 +73,7 @@ export default {
   },
   data() {
     return {
-      color: ''
+      pickerColor: ''
     };
   },
   watch: {
@@ -44,24 +81,50 @@ export default {
       immediate: true,
       handler(value) {
         if (value.startsWith('#')) {
-          this.color = value;
+          this.pickerColor = value;
         }
       }
     }
   },
   computed: {
     displayValue() {
-      return getColorFromName(this.userConfig[this.config.key] || this.config.value, this.golbalColor);
+      return getColorFromName(this.colorValue, this.golbalColor);
+    },
+    colorValue() {
+      return this.userConfig[this.config.key] || this.config.value;
     },
     displayName() {
       return getStyleDisplayName(this.config, this.componentName);
+    },
+    isGlobalColor() {
+      return this.colorValue.startsWith('$');
+    },
+    popoverTrigger() {
+      return this.isGlobalColor ? 'click' : 'manual';
+    },
+    golbalColorList() {
+      return Object.keys(this.golbalColor).map((c) => (
+        {
+          label: getStyleDisplayName(this.golbalColor[c]),
+          color: this.golbalColor[c].value,
+          value: c,
+          colorInvert: invertColor(this.golbalColor[c].value)
+        }
+      ));
     }
   },
   methods: {
+    onListColorClick(e) {
+      this.onChange(e.value);
+      this.$refs.popover && this.$refs.popover.doClose();
+    },
     onPickerChange(e) {
+      this.onChange(e);
+    },
+    onChange(value) {
       this.$emit('onChange', {
         key: this.config.key,
-        value: e
+        value
       });
     }
   }
