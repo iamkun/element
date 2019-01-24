9a32cce01f4362a5438633000934883a3b0ca9ea
iamkun <kunhello@outlook.com>
2019-01-24 17:50:41 +0800

    update logo color

---
+++ examples/components/theme-configurator/editor/sampleEditor.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="config" :key="displayName">
+    fontWeight
+  </section>
+</template>
+
+<style lang="postcss">
+
+</style>
+
+<script>
+import Mixin from './mixin';
+
+export default {
+  props: {
+    config: {
+      type: Object
+    },
+    userConfig: {
+      type: Object
+    },
+    componentName: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      pickerColor: ''
+    };
+  },
+  mixins: [Mixin]
+};
+</script>
