35991253b28fdd7d5f3739da573f6e81414491af
iamkun <kunhello@outlook.com>
2019-01-25 10:54:23 +0800

    update doc style

--- examples/components/theme-configurator/docStyle.vue
+++ examples/components/theme-configurator/docStyle.vue
@@ -16,7 +16,7 @@ export default {
       if (typeof val !== 'string') return;
       const getHandler = (variable, id) => {
         return () => {
-          let newStyle = this.updateStyle(this[variable], oldVal, val);
+          let newStyle = this.updateStyle(this[variable], this.theme, e.global['$--color-primary']);
           updateDomHeadStyle(id, newStyle);
         };
       };
