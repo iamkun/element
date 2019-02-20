d34adba67a90cbda3a4bb1849929f4cd908296dc
iamkun <kunhello@outlook.com>
2019-02-20 11:29:02 +0800

    update api

--- examples/components/theme-configurator/utils/api.js
+++ examples/components/theme-configurator/utils/api.js
@@ -3,9 +3,9 @@ import { post, get } from './ajax';
 const host = process.env.NODE_ENV !== 'production' ? 'http://localhost:3008/' : 'https://ssr.alpha.elenet.me/element-theme-server/';
 
 export const getVars = () => {
-  return get(host);
+  return get(`${host}getVariable`);
 };
 
 export const updateVars = (data, cb) => {
-  return post(host, data, cb);
+  return post(`${host}updateVariable`, data, cb);
 };
