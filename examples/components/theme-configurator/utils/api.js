a05c35b685683ae6c2e99bc92443727edcf7b728
iamkun <kunhello@outlook.com>
2019-02-27 12:07:42 +0800

    add version

--- examples/components/theme-configurator/utils/api.js
+++ examples/components/theme-configurator/utils/api.js
@@ -1,4 +1,7 @@
+import Element from 'main/index.js';
 import { post, get } from './ajax';
+
+const { version } = Element;
 
 const hostList = {
   local: 'http://localhost:3008/',
@@ -9,9 +12,9 @@ const hostList = {
 const host = hostList[process.env.FAAS_ENV] || hostList.production;
 
 export const getVars = () => {
-  return get(`${host}getVariable`);
+  return get(`${host}getVariable?version=${version}`);
 };
 
 export const updateVars = (data, cb) => {
-  return post(`${host}updateVariable`, data, cb);
+  return post(`${host}updateVariable?version=${version}`, data, cb);
 };
